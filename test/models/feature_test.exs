9c513cb06859cd951486e620f8b0d42fa6a59a41
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-07-25 17:46:25 -0700

    Autoset Feature mapping from name

--- test/models/feature_test.exs
+++ test/models/feature_test.exs
@@ -3,8 +3,9 @@ defmodule Tater.FeatureTest do
 
   alias Tater.Feature
 
-  @valid_attrs %{annotation: "some content", mapping: "some content", name: "some content"}
-  @invalid_attrs %{}
+  @valid_attrs %{annotation: "some content", mapping: "some-content",
+                 name: "some content"}
+  @invalid_attrs %{name: ""}
 
   test "changeset with valid attributes" do
     changeset = Feature.changeset(%Feature{}, @valid_attrs)
@@ -14,5 +15,40 @@ defmodule Tater.FeatureTest do
   test "changeset with invalid attributes" do
     changeset = Feature.changeset(%Feature{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  describe "auto mapping" do
+    test "generates one when blank" do
+      changeset = Feature.changeset(%Feature{}, %{name: "Hero", mapping: ""})
+      assert fetch_field(changeset, :mapping) == {:changes, "hero"}
+    end
+
+    test "generates one when not changing mapping" do
+      changeset = Feature.changeset(%Feature{},
+                                    %{name: "Hero", annotation: "some content"})
+      assert fetch_field(changeset, :mapping) == {:changes, "hero"}
+    end
+
+    test "substitutes spaces for dashes" do
+      changeset = Feature.changeset(%Feature{}, %{name: "Supa Hero"})
+      assert fetch_field(changeset, :mapping) == {:changes, "supa-hero"}
+    end
+
+    test "trims leading and trailing spaces" do
+      changeset = Feature.changeset(%Feature{}, %{name: " Supa  Hero  "})
+      assert fetch_field(changeset, :mapping) == {:changes, "supa-hero"}
+    end
+
+    test "doesn't auto map when getting changed" do
+      changeset = Feature.changeset(%Feature{},
+                                    %{name: "Hero", mapping: "custom"})
+      assert fetch_field(changeset, :mapping) == {:changes, "custom"}
+    end
+
+    test "when auto mapping is already taken, appends '-2'" do
+      Repo.insert! %Feature{mapping: "hero"}
+      changeset = Feature.changeset(%Feature{}, %{name: "Hero"})
+      assert fetch_field(changeset, :mapping) == {:changes, "hero-2"}
+    end
   end
 end
--- web/models/feature.ex
+++ web/models/feature.ex
@@ -1,6 +1,7 @@
 defmodule Tater.Feature do
   use Tater.Web, :model
 
+  # TODO: validate mapping (no spaces, length, unique)
   schema "features" do
     field :name, :string
     field :mapping, :string
@@ -15,6 +16,42 @@ defmodule Tater.Feature do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :mapping, :annotation])
+    |> auto_map
     |> validate_required([:name, :mapping, :annotation])
+  end
+
+  # when mapping is being changed
+  defp auto_map(%Ecto.Changeset{changes: %{name: name, mapping: mapping}} = changeset) do
+    changeset
+  end
+
+  # when no mapping
+  defp auto_map(%Ecto.Changeset{changes: %{name: name}} = changeset) do
+    changeset |> put_change(:mapping, first_available_mapping(name))
+  end
+
+  defp auto_map(changeset) do
+    changeset
+  end
+
+  defp mapping_option(name) do
+    name
+    |> String.trim
+    |> String.downcase
+    |> String.replace(~r/\s+/, "-")
+  end
+
+  defp mapping_option(name, index) do
+    "#{mapping_option(name)}-#{index + 1}"
+  end
+
+  defp first_available_mapping(name) do
+    # TODO: loop through test mappings instead of just using index
+    mapping = mapping_option(name)
+    query = from f in Tater.Feature, where: f.mapping == ^mapping
+    case query |> Tater.Repo.all do
+      [] -> mapping
+      matches -> mapping_option(mapping, length(matches))
+    end
   end
 end
