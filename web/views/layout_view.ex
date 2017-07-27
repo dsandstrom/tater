1b3a583085d99e64695f2e85aeea5773ef2c269a
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-07-27 10:45:42 -0700

    Add show_flash helper to show flash notices

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,19 @@
 defmodule Tater.LayoutView do
+  @moduledoc """
+  Conveniences for layout views.
+  """
+
   use Tater.Web, :view
+
+  def show_flash(conn, type) do
+    if get_flash(conn, type) != nil do
+      flash_message(type, get_flash(conn, type))
+    end
+  end
+
+  defp flash_message(type, message) do
+    content_tag :div, class: "alert alert-#{type}" do
+      content_tag(:p, message)
+    end
+  end
 end
