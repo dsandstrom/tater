5f6df60932be3908830340cc273b0670249308d0
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-07-28 12:14:25 -0700

    Add feature partial

--- web/views/feature_view.ex
+++ web/views/feature_view.ex
@@ -1,3 +1,19 @@
 defmodule Tater.FeatureView do
+  @moduledoc """
+  Helpers for feature views
+  """
+
   use Tater.Web, :view
+
+  def render_features(features = [], _), do: content_tag(:p, "None yet")
+  # TODO: pass `feature` variable to partial
+  # get compilation error saying undefined feature
+  def render_features(features, assigns) do
+    content_tag :div, id: "features", class: "features" do
+      for feature <- features do
+        assigns = Map.put_new(assigns, :feature, feature)
+        render "_feature.html", assigns
+      end
+    end
+  end
 end
