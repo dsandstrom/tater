25c8aa066cf0a7026c5b3863cc92fd3932d0326c
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-08-02 16:17:45 -0700

    Add sass loader to webpack
    It's not compiling to app.css yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-var CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./web/static/js/app.js",
+  entry: ["./web/static/js/app.js", "./web/static/css/app.scss"],
   output: {
-    path: path.resolve(__dirname, "priv/static/js"),
-    filename: "app.js"
+    path: path.resolve(__dirname, "priv/static"),
+    filename: "js/app.js"
   },
 
   resolve: {
@@ -24,6 +24,19 @@ module.exports = {
       query: {
         presets: ["es2015"]
       }
+    }],
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader", // compiles Sass to CSS
+        options: {
+          includePaths: ["node_modules"]
+        }
+      }]
     }]
   }
 };
