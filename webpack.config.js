61b93c5d6c76f3c6e3e51238c167b5b9d9d19775
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-08-02 16:25:41 -0700

    Make webpack compile scss to app.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: ["./web/static/js/app.js", "./web/static/css/app.scss"],
@@ -27,16 +28,21 @@ module.exports = {
     }],
     rules: [{
       test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader", // compiles Sass to CSS
-        options: {
-          includePaths: ["node_modules"]
-        }
-      }]
+      use: ExtractTextPlugin.extract({
+        use: [{
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader", // compiles Sass to CSS
+          options: {
+            includePaths: ["node_modules"]
+          }
+        }],
+        fallback: "style-loader" // use style-loader in development
+      })
     }]
-  }
+  },
+
+  plugins: [
+    new ExtractTextPlugin("css/app.css")
+  ]
 };
