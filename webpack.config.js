3f9e7b166336f8c54a9b5a34337b97aad4a43df8
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-08-02 12:13:13 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: "/Users/dsandstrom/Projects/elixir/tater/web/static/js/app.js",
+  output: {
+    path: "/Users/dsandstrom/Projects/elixir/tater/priv/static/js",
+    filename: "app.js"
+  }
+};
