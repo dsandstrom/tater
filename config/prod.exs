d78464dfa3baba78a085e70f40760dfb0dda00f9
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-08-09 16:50:38 -0700

    Set prod settings for server

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tater, Tater.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "127.0.0.1", port: 6001],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
