e8514dc61782a770f81a9be8bf75b32654e8ab14
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-08-09 10:59:06 -0700

    Add release config file

---
+++ rel/config.exs
@@ -0,0 +1,47 @@
+# Import all plugins from `rel/plugins`
+# They can then be used by adding `plugin MyPlugin` to
+# either an environment, or release definition, where
+# `MyPlugin` is the name of the plugin module.
+Path.join(["rel", "plugins", "*.exs"])
+|> Path.wildcard()
+|> Enum.map(&Code.eval_file(&1))
+
+use Mix.Releases.Config,
+    # This sets the default release built by `mix release`
+    default_release: :default,
+    # This sets the default environment used by `mix release`
+    default_environment: Mix.env()
+
+# For a full list of config options for both releases
+# and environments, visit https://hexdocs.pm/distillery/configuration.html
+
+
+# You may define one or more environments in this file,
+# an environment's settings will override those of a release
+# when building in that environment, this combination of release
+# and environment configuration is called a profile
+
+environment :dev do
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"m1?|_hy!F5N3=t1`O:9Qzq0ARG.Pp}`JlN!LK?:c)3;0I2m=%(Cki4DkCX2h&}8&"
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+  set cookie: :"fm3qQE7!.ltGADnAjY>X`n,fq2;I5()hV$PO}MNygw2@Sn1,*$ZYJKoLN$V{WSSm"
+end
+
+# You may define one or more releases in this file.
+# If you have not set a default release, or selected one
+# when running `mix release`, the first release in the file
+# will be used by default
+
+release :tater do
+  set version: current_version(:tater)
+  set applications: [
+    :runtime_tools
+  ]
+end
+
