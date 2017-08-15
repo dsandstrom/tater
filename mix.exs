7ba98e5da288ae2c974cd19a0ffe9ad4c4574bca
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2017-08-15 16:31:48 -0700

    Bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tater.Mixfile do
 
   def project do
     [app: :tater,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
